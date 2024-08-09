2a4444c05b93fa2878889935541ca7d9665c42ea
Kumo1e <so885617@gmail.com>
2024-08-09 10:43:55 +0800

    [refactor] flask app, handle secret key and access token

--- flask_linebot_webhook_example.py
+++ flask_linebot_webhook_example.py
@@ -18,31 +18,20 @@ from linebot.v3.webhooks import (
     MessageEvent,       # 傳輸過來的方法
     TextMessageContent  # 使用者傳過來的資料格式
 )
-import os, sys
+from get_handle_keys import get_secret_and_token
+
 
 app = Flask(__name__)
+keys = get_secret_and_token()
+handler = WebhookHandler(keys["LINEBOT_SECRET_KEY"])
+configuration = Configuration(access_token=keys["LINEBOT_ACCESS_TOKEN"])
 
-# 從環境變數取得 secret key & access token
-channel_secret = os.getenv('LINEBOT_SECRET_KEY', None)
-channel_access_token = os.getenv('LINEBOT_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINEBOT_SECRET_KEY as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINEBOT_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-
-configuration = Configuration(access_token=channel_access_token)
-handler = WebhookHandler(channel_secret)
-
-
-# 設計一個callback的路由，用來提供給Line的官方後台去呼叫
-# 也就是所謂的Webhook Server
-# 官方會將使用者傳的訊息轉傳給Webhook Server
-# 所以會使用 RESTful API 的 POST 方法
 @app.route("/callback", methods=['POST'])
 def callback():
+    # 設計一個callback的路由，用來提供給Line的官方後台去呼叫
+    # 也就是所謂的Webhook Server
+    # 官方會將使用者傳的訊息轉傳給Webhook Server
+    # 所以會使用 RESTful API 的 POST 方法
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -59,17 +48,17 @@ def callback():
 
     return 'OK'
 
-# 測試用，確定 webhook server 有連通
 @app.route("/")
 def say_hello(username=None):
+    # 測試用，確定 webhook server 有連通
     return render_template("hello.html",name=username)
 
-# 根據不同的使用者事件(Event)，用不同的方式回應
-# eg. MessageEvent 代表使用者傳輸訊息(包含 純文字、圖片、聲音、貼圖...)
-# TextMessageContent 代表使用者傳輸的訊息內容是文字
-# 符合兩者條件的事件會被handler_message 所處理
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
+    # 根據不同的使用者事件(Event)，用不同的方式回應
+    # eg. MessageEvent 代表使用者傳輸訊息(包含 純文字、圖片、聲音、貼圖...)
+    # TextMessageContent 代表使用者傳輸的訊息內容是文字
+    # 符合兩者條件的事件會被handler_message 所處理
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
---
+++ get_handle_keys.py
@@ -0,0 +1,17 @@
+import os, sys 
+
+def get_secret_and_token():
+    # 從環境變數取得 secret key & access token
+    channel_secret = os.getenv('LINEBOT_SECRET_KEY', None)
+    channel_access_token = os.getenv('LINEBOT_ACCESS_TOKEN', None)
+    if channel_secret is None:
+        print('Specify LINEBOT_SECRET_KEY as environment variable.')
+        sys.exit(1)
+    if channel_access_token is None:
+        print('Specify LINEBOT_ACCESS_TOKEN as environment variable.')
+        sys.exit(1)
+    
+    return {
+        "LINEBOT_SECRET_KEY": channel_secret,
+        "LINEBOT_ACCESS_TOKEN": channel_access_token
+    }
