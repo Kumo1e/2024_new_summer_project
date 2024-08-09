73bdb168f701b8d0b6cc4c0ae26f0b2e659512f8
Kumo1e <so885617@gmail.com>
2024-08-09 16:52:52 +0800

    [refactor] Linebot with ChatGPT

--- flask_linebot_webhook_example.py
+++ flask_linebot_webhook_example.py
@@ -19,12 +19,13 @@ from linebot.v3.webhooks import (
     TextMessageContent  # 使用者傳過來的資料格式
 )
 from get_handle_keys import get_secret_and_token
-
+from openai_api import chat_with_gpt
 
 app = Flask(__name__)
 keys = get_secret_and_token()
 handler = WebhookHandler(keys["LINEBOT_SECRET_KEY"])
 configuration = Configuration(access_token=keys["LINEBOT_ACCESS_TOKEN"])
+api_key = access_token=keys["OPENAI_API_KEY"]
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -59,12 +60,14 @@ def handle_message(event):
     # eg. MessageEvent 代表使用者傳輸訊息(包含 純文字、圖片、聲音、貼圖...)
     # TextMessageContent 代表使用者傳輸的訊息內容是文字
     # 符合兩者條件的事件會被handler_message 所處理
+    response = chat_with_gpt(event.message.text, api_key)
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
+        
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
+                messages=[TextMessage(text=response)]
             )
         )
 
