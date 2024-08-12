81eb991829141ecfb0f581003fa4314c7a7f0e77
Kumo1e <so885617@gmail.com>
2024-08-12 16:36:28 +0800

    [refactor]

--- cwa_opendata_scraper.py
+++ cwa_opendata_scraper.py
@@ -1,35 +1,41 @@
 from pprint import pprint 
-import requests
-import json
-import os
-
-hearder = {
-    "Accept":  'application/json'
-}
-parameters = {
-    'Authorization': os.getenv("CWA_KEY", None),
-    'locationName': ["臺中市", "新竹縣", "新竹市", "臺南市"]
-}
-
-url = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001'
-
-response = requests.get(url, headers=hearder, params=parameters)
-if response.status_code == 200:
-    weather = response.json()
-    # pprint(weather['records']['location'])
-else:
-    print("Connect False")
+import requests, json, os
 
 weather_element_name = {
-    'CI': '舒適度',
-    'MaxT': '最高溫度',
-    'MinT': '最低溫度',
-    'PoP': '降雨機率',
-    'Wx': '天氣現象'
-}
+        'CI': '舒適度',
+        'MaxT': '最高溫度',
+        'MinT': '最低溫度',
+        'PoP': '降雨機率',
+        'Wx': '天氣現象'
+    }
 
-cities_weather = dict()
-for location in weather['records']['location']:
+def get_cyties_weather(cwa_key: str, location_name: list) -> dict:
+
+    hearder = {
+        "Accept":  'application/json'
+    }
+    parameters = {
+        'Authorization': cwa_key,  
+        'locationName': location_name
+    }
+
+    url = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001'
+
+    response = requests.get(url, headers=hearder, params=parameters)
+    if response.status_code == 200:
+        weather = response.json()
+    else:
+        print("Connect False")
+
+    cities_weather = dict()
+    for location in weather['records']['location']:
+        city_weather = dict()
+        city_weather = get_city_weather(location)
+        cities_weather[location['locationName']] = city_weather
+
+    return cities_weather
+
+def get_city_weather(location):
     city_weather = dict()
     for weather_element in location['weatherElement']:
         element_name = weather_element["elementName"]
@@ -42,7 +48,14 @@ for location in weather['records']['location']:
 
         element_value = weather_element["time"][0]["parameter"]["parameterName"]
         city_weather[weather_element_name[element_name]] = element_value + parameterUnit
-    
-    cities_weather[location['locationName']] = city_weather
 
-pprint(cities_weather)
+    return city_weather
+
+
+if __name__ == "__main__":
+    cwa_key = os.getenv("CWA_KEY", None)
+    location_name = ["臺中市", "新竹縣", "新竹市", "臺南市"]
+    if cwa_key:
+        pprint(get_cyties_weather(cwa_key, location_name))
+    else:
+        print("CWA API key is not found")
