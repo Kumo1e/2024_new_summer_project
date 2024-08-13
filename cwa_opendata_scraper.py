a4e91ce40d8d9c3cb32a1f2fcae631cb77117de3
Kumo1e <so885617@gmail.com>
2024-08-13 15:34:32 +0800

    [refactor] identify similar word

--- cwa_opendata_scraper.py
+++ cwa_opendata_scraper.py
@@ -9,7 +9,24 @@ weather_element_name = {
         'Wx': '天氣現象'
     }
 
+# taiwan_counties = [
+#     "台北市", "新北市", "桃園市", "台中市", "台南市", "高雄市",
+#     "宜蘭縣", "新竹縣", "苗栗縣", "彰化縣", "南投縣", "雲林縣", 
+#     "嘉義縣", "屏東縣", "花蓮縣", "台東縣", "澎湖縣", "基隆市", 
+#     "新竹市", "嘉義市", "臺中市", "臺北市", "臺南市", "臺東市",
+#     "金門縣", "連江縣"]
+
 def get_cyties_weather(cwa_key: str, location_name: list) -> dict:
+    variant_characters = {
+        "台北市": "臺北市",
+        "台中市": "臺中市",
+        "台南市": "臺南市",
+        "台東市": "臺東市"
+    }
+
+    for i in range(len(location_name)):
+        if location_name[i] in variant_characters.keys():
+            location_name[i] = variant_characters[location_name[i]]
 
     hearder = {
         "Accept":  'application/json'
@@ -54,7 +71,7 @@ def get_city_weather(location):
 
 if __name__ == "__main__":
     cwa_key = os.getenv("CWA_KEY", None)
-    location_name = ["臺中市", "新竹縣", "新竹市", "臺南市"]
+    location_name = ["臺中市", "新竹縣", "新竹市", "台南市"]
     if cwa_key:
         pprint(get_cyties_weather(cwa_key, location_name))
     else:
